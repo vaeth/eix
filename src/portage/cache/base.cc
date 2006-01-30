d364ef79e0452d29873d5ff861259e39bca27a31
Emil Beinroth <emilbeinroth at gmx.net>
2006-01-30 15:01:09 +0000

    Moved inline-functions from header to cc-file.
    git-svn-id: https://svn.gentooexperimental.org/eix/trunk@64 3c3e6e0f-8f03-0410-a2c1-9066eb21f691

---
+++ src/portage/cache/base.cc
@@ -0,0 +1,71 @@
+/***************************************************************************
+ *   eix is a small utility for searching ebuilds in the                   *
+ *   Gentoo Linux portage system. It uses indexing to allow quick searches *
+ *   in package descriptions with regular expressions.                     *
+ *                                                                         *
+ *   https://sourceforge.net/projects/eix                                  *
+ *                                                                         *
+ *   Copyright (c)                                                         *
+ *     Wolfgang Frisch <xororand@users.sourceforge.net>                    *
+ *     Emil Beinroth <emilbeinroth@gmx.net>                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "base.h"
+
+Package *
+addPackage(vector<Package*> &v, const string &cat, const string pkg)
+{
+	Package *p = new Package(cat, pkg);
+	OOM_ASSERT(p);
+	v.push_back(p);
+	return p;
+}
+
+Package *
+findPackage(vector<Package*> &v, const char *pkg)
+{
+	Package *ret = NULL;
+	for(vector<Package*>::iterator i = v.begin();
+		i != v.end();
+		++i)
+	{
+		if((*i)->name == pkg) {
+			ret = *i;
+			break;
+		}
+	}
+	return ret;
+}
+
+bool
+deletePackage(vector<Package*> &v, string pkg)
+{
+	bool ret = false;
+	for(vector<Package*>::iterator i = v.begin();
+		i != v.end();
+		++i)
+	{
+		if((*i)->name == pkg) {
+			delete *i;
+			v.erase(i);
+			ret = true;
+			break;
+		}
+	}
+	return false;
+}
