82e5177ea98fa5c03fcc5a04e52ae550a5f92ad1
Emil Beinroth <emilbeinroth at gmx.net>
2006-02-09 13:23:42 +0000

    Added metadata as cache-method.
    git-svn-id: https://svn.gentooexperimental.org/eix/trunk@80 3c3e6e0f-8f03-0410-a2c1-9066eb21f691

---
+++ src/portage/cache/metadata/metadata.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   eix is a small utility for searching ebuilds in the                   *
+ *   Gentoo Linux portage system. It uses indexing to allow quick searches *
+ *   in package descriptions with regular expressions.                     *
+ *                                                                         *
+ *   https://sourceforge.net/projects/eix                                  *
+ *                                                                         *
+ *   Copyright (c)                                                         *
+ *     Wolfgang Frisch <xororand@users.sourceforge.net>                    *
+ *     Emil Beinroth <emilbeinroth@gmx.net>                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef __METADATA_H__
+#define __METADATA_H__
+
+#include <portage/cache/base.h>
+
+class MetadataCache : public BasicCache {
+
+	public:
+		int readCategory(vector<Package*> &vec, const string &cat_name);
+
+		const char *getType() const {
+			return "metadata";
+		}
+};
+
+#endif /* __METADATA_H__ */
