455150a9f2fbd893111c94ecb45bdfd3d4c2d1d4
Emil Beinroth <emilbeinroth at gmx.net>
2007-10-04 14:07:11 +0000

    Replace full license headers with the following:   This file is part of the eix project and distributed under the terms of the   GNU General Public License v2. This change removes about 2000 useless lines from the project.
    git-svn-id: https://svn.gentooexperimental.org/eix/trunk@467 3c3e6e0f-8f03-0410-a2c1-9066eb21f691

--- src/eixTk/levenshtein.h
+++ src/eixTk/levenshtein.h
@@ -1,22 +1,8 @@
-/***************************************************************************
- *   Copyright (C) 2005 by Wolfgang Frisch                                 *
- *   xororand@users.sf.net                                                 *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+// This file is part of the eix project and distributed under the
+// terms of the GNU General Public License v2.
+//
+// Copyright (c)                                                         
+//   Wolfgang Frisch <xororand@users.sourceforge.net>                    
 
 #ifndef __LEVENSHTEIN_H__
 #define __LEVENSHTEIN_H__
