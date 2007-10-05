ada13f9ac6e1593b58acad1077669859eb4d3ac8
Emil Beinroth <emilbeinroth at gmx.net>
2007-10-05 10:53:36 +0000

    Add ICC to list of compilers.
    git-svn-id: https://svn.gentooexperimental.org/eix/trunk@473 3c3e6e0f-8f03-0410-a2c1-9066eb21f691

--- contrib/compile-test.sh
+++ contrib/compile-test.sh
@@ -19,20 +19,38 @@ set_status() {
 
 gcc=($(get_avail_gcc))
 gcc_faild=(${gcc[@]})
+
+if which icpc &>/dev/null; then
+	# we have ICC installed, try it
+	for ((gcc_n=0;gcc_n<"${#gcc[@]}";++gcc_n)); do
+		if [[ "${gcc[$gcc_n]}" != *"-4."* ]]; then
+			for ((gcc_i=$[${#gcc[@]}-1];gcc_i>$gcc_n;--gcc_i)); do
+				gcc[$[$gcc_i+1]]=${gcc[$gcc_i]}
+			done
+			gcc[$[$gcc_n+1]]=$(which icpc)
+			break
+		fi
+	done
+fi
+
 original_gcc=$(gcc-config -c)
 trap reset_gcc EXIT INT
-
 
 for ((gcc_n=0;gcc_n<"${#gcc[@]}";++gcc_n)); do
 	make maintainer-clean &>/dev/null
 	echo "trying ${gcc[$gcc_n]}"
 
-	if ! gcc-config "${gcc[$gcc_n]}"; then
-		set_status $gcc_n "(1 of 5) gcc-config failed"
-		continue
-	fi
+	unset CXX
 
-	source /etc/profile
+	if [[ "${gcc[$gcc_n]:0:1}" != '/' ]]; then
+		if ! gcc-config "${gcc[$gcc_n]}"; then
+			set_status $gcc_n "(1 of 5) gcc-config failed"
+			continue
+		fi
+		source /etc/profile
+	else
+		export CXX="${gcc[$gcc_n]}"
+	fi
 
 	if ! ./autogen.sh; then
 		set_status $gcc_n "(2 of 5) autogen.sh failed"
