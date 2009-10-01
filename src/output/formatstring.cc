8abf3fdc8ac3f3ab2b8dbb0cbe18ddbf4de7754e
Martin Vaeth <vaeth at mathematik.uni-wuerzburg.de>
2009-10-01 19:49:54 +0000

    Fix stupid typo in my previous checkin
    git-svn-id: https://svn.gentooexperimental.org/eix/trunk@938 3c3e6e0f-8f03-0410-a2c1-9066eb21f691

--- src/output/formatstring.cc
+++ src/output/formatstring.cc
@@ -524,7 +524,7 @@ FormatParser::state_IF()
 	if(*band_position == '}') {
 		n->text = Text("");
 		n->negation = !n->negation;
-		n->rhs = ConditionBlock::RHS_String;
+		n->rhs = ConditionBlock::RHS_STRING;
 		++band_position;
 		return START;
 	}
