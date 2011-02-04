de8781442c897a205e7e166cf9c8702d5dc82770
Martin Vaeth <vaeth at mathematik.uni-wuerzburg.de>
2011-02-04 23:40:33 +0000

    Remove --with-rst, add --extra-doc, ship *.html in tarball
    git-svn-id: https://svn.gentooexperimental.org/eix/trunk@1119 3c3e6e0f-8f03-0410-a2c1-9066eb21f691

---
+++ doc/call_rst2html.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+if command -v rst2html.py >/dev/null 2>&1
+then	rst2html.py --embed-stylesheet --stylesheet-path=stylesheet.css -- "${1}" "${2}"
+	exit
+fi
+if test -s "${2}"
+then	echo "warning: using existing ${2} since rst2html.py is not available" >&2
+	touch "${2}" || : >>"${2}"
+	exit
+else	echo "error: ${2} does not exist and rst2html.py is not available" >&2
+	exit 1
+fi
