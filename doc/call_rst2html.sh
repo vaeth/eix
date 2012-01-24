a17afd78237d4057b111959c03bae76de20ee2f0
Martin Väth <vaeth@mathematik.uni-wuerzburg.de>
2012-01-24 15:24:24 +0100

    Bring back utf-8 to docs.

--- doc/call_rst2html.sh
+++ doc/call_rst2html.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
 if command -v rst2html.py >/dev/null 2>&1
-then	rst2html.py --embed-stylesheet --stylesheet-path=stylesheet.css -- "${1}" "${2}"
+then	rst2html.py \
+		'--input-encoding=UTF-8:strict' \
+		'--embed-stylesheet' \
+		'--stylesheet-path=stylesheet.css' \
+		-- "${1}" "${2}"
 	exit
 fi
-if test -s "${2}"
+if test -r "${2}"
 then	echo "warning: using existing ${2} since rst2html.py is not available" >&2
 	touch "${2}" || : >>"${2}"
 	exit
